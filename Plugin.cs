f5c9e8b1bdeaf0fe6b65d1d4b61ee61f5ecf44be
imxzjv <20950035+imxzjv@users.noreply.github.com>
2016-05-21 00:30:28 +0000

    * Wizard: Spam ET fix.

--- Plugin.cs
+++ Plugin.cs
@@ -65,7 +65,7 @@ namespace Trinity
                 if (_version != null) return _version;
                 var verXml = XDocument.Load(FileManager.VersionPath).Descendants("Revision").FirstOrDefault();
                 if (verXml != null) return new Version(2,41, int.Parse(verXml.Value));
-                return new Version(2, 41, 0);
+                return new Version(2, 42, 0);
             }
         }
 
