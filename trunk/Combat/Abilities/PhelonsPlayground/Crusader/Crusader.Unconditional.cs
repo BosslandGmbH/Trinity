895aec1894fce92b978cdcbfb799fecb2792b5d4
ph3l0n <ph3l0n@8b886bb1-50ed-47f6-93b4-c619e4d10bba>
2016-05-23 23:27:16 +0000

    *Added Support for Laws of Hope out of Combat.

--- trunk/Combat/Abilities/PhelonsPlayground/Crusader/Crusader.Unconditional.cs
+++ trunk/Combat/Abilities/PhelonsPlayground/Crusader/Crusader.Unconditional.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Trinity.Config.Combat;
+using Trinity.Movement;
 using Trinity.Reference;
 using Zeta.Common;
 using Zeta.Game;
@@ -20,7 +21,26 @@ namespace Trinity.Combat.Abilities.PhelonsPlayground.Crusader
                 //Cast Akarats any time to make sure we do not die
                 if (ShouldAkaratsChampion)
                     return CastAkaratsChampion;
+                if (ShouldLawsOfHope)
+                    return CastLawsOfHope;
                 return null;
+            }
+
+            private static TrinityPower CastLawsOfHope
+            {
+                get { return new TrinityPower(Skills.Crusader.LawsOfHope.SNOPower); }
+            }
+
+            private static bool ShouldLawsOfHope
+            {
+                get
+                {
+                    return Skills.Crusader.LawsOfHope.CanCast() &&
+                           !CacheData.Buffs.HasBuff(SNOPower.X1_Crusader_LawsOfHope2, 6) &&
+                           (!IsSteedCharging && CurrentTarget == null || !CurrentTarget.IsUnit) &&
+                           Settings.Combat.Crusader.SpamLaws;
+                    ;
+                }
             }
 
             private static bool ShouldAkaratsChampion
@@ -28,36 +48,42 @@ namespace Trinity.Combat.Abilities.PhelonsPlayground.Crusader
                 get
                 {
                     //Basic checks
-                    if (!Skills.Crusader.AkaratsChampion.CanCast() || Player.IsInTown)
+                    if (!Skills.Crusader.AkaratsChampion.CanCast())
                         return false;
 
                     // Akarat's mode is 'Off Cooldown'
                     if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.WhenReady)
                         return true;
-
-                    // Let's check for Goblins, Current Health, CDR Pylon, movement impaired
-                    if (CurrentTarget != null && CurrentTarget.IsTreasureGoblin &&
-                        Settings.Combat.Monk.UseEpiphanyGoblin ||
-                        Player.CurrentHealthPct <= 0.39 && Settings.Combat.Crusader.AkaratsEmergencyHealth ||
-                        GetHasBuff(SNOPower.Pages_Buff_Infinite_Casting) ||
-                        Settings.Combat.Crusader.AkaratsOnStatusEffect &&
+                    //Use on Low Health
+                    if (Player.CurrentHealthPct <= 0.25 &&
+                        (Settings.Combat.Crusader.AkaratsEmergencyHealth || Runes.Crusader.Prophet.IsActive))
+                        return true;
+                    //Use if Incapacitated
+                    if (Settings.Combat.Crusader.AkaratsOnStatusEffect &&
                         (ZetaDia.Me.IsFrozen || ZetaDia.Me.IsRooted || ZetaDia.Me.IsFeared || ZetaDia.Me.IsStunned))
                         return true;
 
-                    // Akarat's mode is 'Whenever in Combat'
-                    if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.WhenInCombat &&
-                        TargetUtil.AnyMobsInRange(80f))
-                        return true;
+                    if (!IsSteedCharging || ClassMover.HasInfiniteCasting)
+                    {
+                        // Let's check for Goblins, Current Health, CDR Pylon, movement impaired
+                        if (CurrentTarget != null && CurrentTarget.IsTreasureGoblin)
+                            return true;
 
-                    // Akarat's mode is 'Use when Elites are nearby'
-                    if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.Normal &&
-                        TargetUtil.AnyElitesInRange(80f))
-                        return true;
+                        // Akarat's mode is 'Whenever in Combat'
+                        if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.WhenInCombat &&
+                            TargetUtil.AnyMobsInRange(40f))
+                            return true;
 
-                    // Akarat's mode is 'Hard Elites Only'
-                    if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.HardElitesOnly && HardElitesPresent)
-                        return true;
+                        // Akarat's mode is 'Use when Elites are nearby'
+                        if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.Normal &&
+                            TargetUtil.AnyElitesInRange(40f))
+                            return true;
 
+                        // Akarat's mode is 'Hard Elites Only'
+                        if (Settings.Combat.Crusader.AkaratsMode == CrusaderAkaratsMode.HardElitesOnly &&
+                            HardElitesPresent)
+                            return true;
+                    }
                     return false;
                 }
             }
