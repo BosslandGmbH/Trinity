8a484cbaea3604c6b4a09a75e382e523cfe64d2f
imxzjv <20950035+imxzjv@users.noreply.github.com>
2016-06-14 03:03:57 +0000

    * Fix for exception in avoidance.

--- Cache/TrinityCacheObject.cs
+++ Cache/TrinityCacheObject.cs
@@ -173,7 +173,8 @@ namespace Trinity
         public bool IsRare { get; set; }        
         public bool IsUnique { get; set; }        
         public bool IsMinion { get; set; }        
-        public MonsterAffixes MonsterAffixes { get; set; }        
+        public MonsterAffixes MonsterAffixes { get; set; }
+        public HashSet<MonsterAffixes> MonsterAffixesCollection { get; set; }
         public bool IsTreasureGoblin { get; set; }
         public bool IsBoss { get; set; }        
         public bool IsAncient { get; set; }              
