331fb3300716133e7cf35e6a81c7c1dee0867160
imxzjv <imxzjv@8b886bb1-50ed-47f6-93b4-c619e4d10bba>
2016-07-01 07:19:57 +0000

    Missing Files.

---
+++ trunk/Framework/Helpers/Exporter/IStructureExporter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Trinity.Framework.Helpers.Exporter
+{
+    public interface IStructureExporter
+    {
+        string Create(IDictionary<string, string> input, ExportOptions options);
+    }
+}
---
+++ trunk/Framework/Objects/Enums/WorldMarkerType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity.Framework.Objects.Enums
+{
+    public enum WorldMarkerType
+    {
+        Hidden = -1,
+        None = 0,
+        Waypoint = 129569,
+        Entrance = 102321,
+        Exit = 102320,
+        Objective = 81058,
+        Portal = 332286
+    }
+}
+
