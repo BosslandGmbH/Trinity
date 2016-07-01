adc0562e92a2c69ebafdbc99f9e5e4df139436cf
imxzjv <20950035+imxzjv@users.noreply.github.com>
2016-07-01 07:19:57 +0000

    Missing Files.

---
+++ Framework/Objects/Enums/TeamType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity.Framework.Objects.Enums
+{
+    public enum TeamType
+    {
+        None = 0,
+        Player = 2,
+        Monsters = 10,
+    }
+}
