188f3b240025bb142516f36e19f860471a208bcc
imxzjv <20950035+imxzjv@users.noreply.github.com>
2016-10-08 05:33:41 +0000

    * Fix for old settings copying nulls on-top of newly added setting defaults.

--- Plugin.cs
+++ Plugin.cs
@@ -32,7 +32,7 @@ namespace Trinity
     public class TrinityPlugin : IPlugin
     {
         public string Name => "Trinity";
-        public Version Version => new Version(2, 55, 665);
+        public Version Version => new Version(2, 55, 666);
         public string Author => "xzjv, TarasBulba, rrrix, jubisman, Phelon and many more";
         public string Description => $"v{Version} provides combat, exploration and much more";
         public Window DisplayWindow => UILoader.GetDisplayWindow(Path.Combine(FileManager.PluginPath, "UI"));
