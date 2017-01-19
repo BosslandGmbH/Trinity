e4f5c6d71ca306ccc914704f6f50150c727e990c
imxzjv <20950035+imxzjv@users.noreply.github.com>
2017-01-19 11:57:53 +0000

    .705 * Added check to prevent casting buffs while interacting with gizmos * Enabled setting to ignore juggernaut affix * Added updated Offsets for patch * Fixed issue with some lore chests being interacted with forever. * ItemList: Fixed barbarian being shown as 'Belt' rather than 'MightyBelt'. * Routines: Arachyr: Fix for settings having wrong filename. * Routines: Crusader Light Hammer: Improvements -TwoCigars * Routines: Updated TwoCigars routines to correct versions. * Routines: Added DemonHunterUnhallowedGrenade by -HitmanReborn

--- Routines/Crusader/CrusaderLightHammer.cs
+++ Routines/Crusader/CrusaderLightHammer.cs
@@ -9,6 +9,7 @@ using Trinity.Framework.Objects;
 using Trinity.Reference;
 using Trinity.UI;
 using Zeta.Common;
+using Zeta.Game.Internals.Actors;
 using Logger = Trinity.Framework.Helpers.Logger;
 
 namespace Trinity.Routines.Crusader
@@ -17,10 +18,10 @@ namespace Trinity.Routines.Crusader
     {
         #region Definition
 
-        public string DisplayName => "Holy Light Hammerdin";
+        public string DisplayName => "Seeker of The Light Hammerdin";
         public string Description => "The Seeker of the Light set marks the return of the Hammerdin build, decidedly focusing on bringing Blessed Hammer Blessed Hammer to competitive DPS in Greater Rifts — aided and protected by Falling Sword Falling Sword. This mobile, sustain damage playstyle is available in both solo progression and speedfarming.";
-        public string Author => "xzjv";
-        public string Version => "0.1";
+        public string Author => "xzjv, TwoCigars";
+        public string Version => "0.5";
         public string Url => "http://www.icy-veins.com/d3/crusader-holy-hammerdin-build-with-blessed-hammer-and-seeker-of-the-light-patch-2-4-2-season-7";
 
         public Build BuildRequirements => new Build
@@ -42,9 +43,6 @@ namespace Trinity.Routines.Crusader
         {
             TrinityPower power;
             TrinityActor target;
-
-            if (!Skills.Crusader.Punish.IsBuffActive && ShouldPunish(out target))
-                return Punish(target);
                 
             if (TrySpecialPower(out power))
                 return power;
@@ -76,17 +74,11 @@ namespace Trinity.Routines.Crusader
             if (AllowedToUse(Settings.Akarats, Skills.Crusader.AkaratsChampion) && ShouldAkaratsChampion())
                 return AkaratsChampion();
 
-            if (ShouldIronSkin())
-                return IronSkin();
-
-            if (ShouldJudgement())
-                return Judgement();
-
-            if (ShouldConsecration())
-                return Consecration();
-
             if (ShouldProvoke())
                 return Provoke();
+
+            if (ShouldIronSkin())
+                return IronSkin();
 
             if (TryLaw(out power))
                 return power;
@@ -103,14 +95,129 @@ namespace Trinity.Routines.Crusader
         {
             Vector3 position;
 
-            // Its not efficient really to falling sword for movement.
-            if (TargetUtil.AnyMobsInRangeOfPosition(destination, 20f, 5) && ShouldFallingSword(out position))
+            //Delay settings in ShouldFallingSword will help keep this from being spammed on a Cooldown Pylon
+            //Default range of 20 yards increased to 50 yards. It will search for any 5 mob cluster within 50 yards
+            if (TargetUtil.AnyMobsInRangeOfPosition(destination, 50f, 5) && ShouldFallingSword(out position))
                 return FallingSword(destination);
 
             if (ShouldSteedCharge())
                 return SteedCharge();
 
             return Walk(destination);
+        }
+
+        protected override bool ShouldFallingSword(out Vector3 position)
+        {
+            position = Vector3.Zero;
+
+            if (!Skills.Crusader.FallingSword.CanCast())
+                return false;
+
+            //If your health falls below the Emergency Health Percentage in Trinity > Routine settings, cast falling sword again regardless of delay setting.
+            if (Player.CurrentHealthPct < Settings.EmergencyHealthPct)
+                return true;
+
+            //Uses the delay [in milliseconds] defined in Trinity > Routines to keep falling sword from being recast too quickly
+            if (Skills.Crusader.FallingSword.TimeSinceUse < Settings.FallingSwordDelay)
+                return false;
+
+            var target = TargetUtil.GetBestClusterUnit() ?? CurrentTarget;
+            if (target != null)
+            {
+                position = target.Position;
+                return true;
+            }
+
+            return false;
+        }
+
+        protected override bool ShouldBlessedHammer(out TrinityActor target)
+        {
+            target = null;
+
+            if (!Skills.Crusader.BlessedHammer.CanCast())
+                return false;
+
+            //Do not cast Blessed Hammer if Wrath is less than Primary Energy Reserve [default = 25]
+            //This will conserve enough energy for Falling Sword to be cast again
+            if (Player.PrimaryResource <= PrimaryEnergyReserve)
+                return false;
+
+            if (!TargetUtil.AnyMobsInRange(10f))
+                return false;
+
+            target = TargetUtil.GetBestClusterUnit() ?? CurrentTarget;
+            return target != null;
+        }
+
+
+        protected override bool ShouldProvoke()
+        {
+            if (!Skills.Crusader.Provoke.CanCast())
+                return false;
+
+            if (Player.HasBuff(SNOPower.X1_Crusader_Provoke))
+                return false;
+
+            if (!TargetUtil.AnyMobsInRange(15f))
+                return false;
+
+            return true;
+        }
+
+        protected override bool ShouldIronSkin()
+        {
+            if (!Skills.Crusader.IronSkin.CanCast())
+                return false;
+
+            if (Player.HasBuff(SNOPower.X1_Crusader_IronSkin))
+                return false;
+
+            if (!TargetUtil.AnyMobsInRange(20f))
+                return false;
+
+            return true;
+        }
+
+        protected override bool ShouldLawsOfValor()
+        {
+            if (!Skills.Crusader.LawsOfValor.CanCast())
+                return false;
+
+            if (!TargetUtil.AnyMobsInRange(25f))
+                return false;
+
+            return true;
+        }
+
+        protected override bool ShouldLawsOfJustice()
+        {
+            if (!Skills.Crusader.LawsOfJustice.CanCast())
+                return false;
+
+            if (!TargetUtil.AnyMobsInRange(25f))
+                return false;
+
+            return true;
+        }
+
+        protected override bool ShouldLawsOfHope()
+        {
+            if (!Skills.Crusader.LawsOfHope.CanCast())
+                return false;
+
+            if (!TargetUtil.AnyMobsInRange(25f))
+                return false;
+
+            return true;
+        }
+
+        protected override bool ShouldAkaratsChampion()
+        {
+            if (!Skills.Crusader.AkaratsChampion.CanCast())
+                return false;
+
+            return true;
         }
 
         #region Settings
@@ -124,6 +231,7 @@ namespace Trinity.Routines.Crusader
         public sealed class CrusaderLightHammerSettings : NotifyBase, IDynamicSetting        
         {
             private SkillSettings _akarats;
+            private int _fallingSwordDelay;
             private int _clusterSize;
             private float _emergencyHealthPct;
 
@@ -146,6 +254,14 @@ namespace Trinity.Routines.Crusader
                 get { return _akarats; }
                 set { SetField(ref _akarats, value); }
             }
+
+            [DefaultValue(2000)]
+            public int FallingSwordDelay
+            {
+                get { return _fallingSwordDelay; }
+                set { SetField(ref _fallingSwordDelay, value); }
+            }
+
 
             #region Skill Defaults
 
