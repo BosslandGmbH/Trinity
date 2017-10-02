5004bacc22c2eb8967582ef544fb3f058b3e35bb
Nesox <Nesox@8b886bb1-50ed-47f6-93b4-c619e4d10bba>
2017-10-02 10:47:42 +0000

    Added another npc to the blacklist. Cleaned up GetShrineType.

--- trunk/Framework/Reference/GameData.cs
+++ trunk/Framework/Reference/GameData.cs
@@ -1885,6 +1885,7 @@ namespace Trinity.Framework.Reference
         /// </summary>
         public static HashSet<int> BlackListIds { get; } = new HashSet<int>
         {
+            (int)SNOActor.x1_NPC_Westmarch_Gorrel_NonUnique,                // NPC for bounty: Noble Deaths.
             (int)SNOActor.bastionsKeepGuard_Melee_A_01_snatched,            // NPC you can't attack.
             (int)SNOActor.bastionsKeepGuard_Melee_A_02_BlazeOfGlory,        // NPC you can't attack.
             (int)SNOActor.Bucket_Crane_caOut_Mining_Breakable,              // Obstacle in Dahlgur Oasis.
