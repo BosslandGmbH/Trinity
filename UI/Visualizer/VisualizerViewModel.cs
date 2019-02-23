c4eaea329af1498d5e2d36d956f9d8262679f292
Dodo <raphudo@users.noreply.github.com>
2019-02-23 16:07:18 +0300

    Removed the need for reference to System.Web. (#102)

--- UI/Visualizer/VisualizerViewModel.cs
+++ UI/Visualizer/VisualizerViewModel.cs
@@ -12,7 +12,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Text;
-using System.Web.UI.WebControls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
